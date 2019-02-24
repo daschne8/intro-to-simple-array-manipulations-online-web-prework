661f76df812dfc0072f965b93a7cabf3c7e04824
Daniel Schneider <daschne8@gmail.com>
2019-02-24 21:15:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array,item)
+  return array.push(item)
+end
+
+def using_unshift(array,item)
+  return array.unshift(item)
+end
+
+def using_pop(array)
+  return array.pop()
+end
+
+def pop_with_args(array,args)
+  return array.pop(2)
+end
+
+def using_insert(array, item)
+  return array.insert(item,4)
+end
