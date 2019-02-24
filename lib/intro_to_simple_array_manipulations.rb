28d3e9babc460eace71ad71f4518f4036465d214
Daniel Schneider <daschne8@gmail.com>
2019-02-24 21:22:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,15 @@ end
 def using_insert(array,element)
   return array.insert(4,element)
 end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array,string)
+  return array.delete(string)
+end
